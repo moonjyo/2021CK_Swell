89eb1d7bb29cbd9cd4066092b83b5c432f70f12c
moonjyo <m_jyo@naver.com>
2021-03-08 19:41:47 +0900

    Scene

---
+++ Assets/Jiho/Script/MonsterBasic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBasic : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 일정 범위를 놓고 레이캐스트를 쏴서 장애물이 아니고 플레이어면 접근 및 공격?
+        Collider[] hitCollider = Physics.OverlapSphere(transform.position, 10.0f);
+        int i = 0;
+        while(hitCollider.Length > i)
+        {
+            i++;
+        }
+    }
+}
