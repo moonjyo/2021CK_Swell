439b0b332870a279626d332bc2b4028548dc3afc
MinhwanKwak <62918501+MinhwanKwak@users.noreply.github.com>
2021-03-09 14:14:19 +0900

    이동 대쉬 구현 완료

--- Assets/Minhwan_Kwak/Script/Player/PlayerInput.cs
+++ Assets/Minhwan_Kwak/Script/Player/PlayerInput.cs
@@ -6,53 +6,51 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     private bool IsRunning = false;
+    private bool IsPress = false;
     private int PressKeyCount;
+
+    [SerializeField]
+    private InputAction InputAction;
+
     public void OnWalk(InputAction.CallbackContext context)
     {
+        
         Vector2 InputValue = context.ReadValue<Vector2>();
-        Vector3 MoveVec = new Vector3(InputValue.x, 0, InputValue.y);
-        PlayerManager.Instance.playerMove.SetMove(MoveVec , IsRunning);
-        //액션이 눌러진상태
-        
-     
-    }
-
-    public void OnWalkPress(InputAction.CallbackContext context)
-    {
-        if (context.performed && context.duration > 0.01f && !IsRunning)
+        Vector3 MoveVec = new Vector3(InputValue.x, 0, 0);
+        PlayerManager.Instance.playerMove.SetMove(MoveVec, IsRunning);
+        if(InputValue.x == 0)
         {
-            IsRunning = false;
-
-            Debug.Log("Walk 종료");
+            Debug.Log("아무것도 누르지않음.");
             PlayerManager.Instance.playerMove.MoveFunction = PlayerManager.Instance.playerMove.Idle;
+            IsPress = false;
         }
-        
-
+        else
+        {
+            Debug.Log("누르는중.");
+            IsPress = true;
+        }
     }
-
-
     public void OnRun(InputAction.CallbackContext context)
     {
-        if(context.performed && !IsRunning && context.duration >= 0.07f)
+        if(context.performed && !IsRunning)
         {
-            IsRunning = true;
-            Debug.Log("Run 시작");
-            return;
-
+                IsRunning = true;
+                Debug.Log("Run 시작");
+                return;
+            
         }
     }
+    [System.Obsolete]
     public void OnRunPress(InputAction.CallbackContext context)
     {
-        if (context.performed && IsRunning && context.duration > 0.1f)
+        if (context.performed && IsRunning && context.duration >= 0.1f)
         {
             IsRunning = false;
-
             Debug.Log("Run 종료");
             PlayerManager.Instance.playerMove.PlayerAnim.SetBool("Run", false);
-            PlayerManager.Instance.playerMove.MoveFunction -= PlayerManager.Instance.playerMove.Idle;
+            PlayerManager.Instance.playerMove.MoveFunction = PlayerManager.Instance.playerMove.Idle;
         }
     }
-
     public void OnAttack(InputAction.CallbackContext context)
     {
         Debug.Log("Attack");
