3c1e6505d02e43afc75ccd2c6c7748b5a5d1ef37
moonjyo <m_jyo@naver.com>
2021-04-17 00:29:20 +0900

    Mouse Input DummyBase

---
+++ Assets/Jiho/Script/MouseInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseInput : MonoBehaviour
+{
+    public void OnLeftMouseButton(InputAction.CallbackContext context)
+    {
+        if(context.started || context.performed)
+        {
+            
+        }
+    }
+
+    public void OnLeftButtonDrag(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            Vector2 value = context.ReadValue<Vector2>();
+            Debug.Log(value);
+        }
+    }
+}
