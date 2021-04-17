f03756dbc05d068c40c6102af51bb20bce4f1cd6
moonjyo <62926522+moonjyo@users.noreply.github.com>
2021-04-17 13:08:42 +0900

    MouseDrag And ObjRotate

--- Assets/Jiho/Script/MouseInput.cs
+++ Assets/Jiho/Script/MouseInput.cs
@@ -6,20 +6,34 @@ using UnityEngine.InputSystem;
 
 public class MouseInput : MonoBehaviour
 {
+
+    public RotateByMouse MouseFunction;
+
+    private bool IsLeftMousePressed;
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        if(context.control.device.GetType() == typeof(Mouse))
+        {
+            if(!IsLeftMousePressed)
+            {
+                return;
+            }
+        }
+
+        Vector2 Input = context.ReadValue<Vector2>();
+        MouseFunction.SetRotateInput(Input);
+    }
+
     public void OnLeftMouseButton(InputAction.CallbackContext context)
     {
         if(context.started || context.performed)
         {
-            
+            IsLeftMousePressed = true;
         }
-    }
-
-    public void OnLeftButtonDrag(InputAction.CallbackContext context)
-    {
-        if(context.started)
+        else
         {
-            Vector2 value = context.ReadValue<Vector2>();
-            Debug.Log(value);
+            IsLeftMousePressed = false;
         }
     }
 }
---
+++ Assets/Jiho/Script/RotateByMouse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateByMouse : MonoBehaviour
+{
+    public float RotHInput;
+    public float RotVInput;
+
+    public GameObject GO;
+
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    public void SetRotateInput(Vector2 value)
+    {
+        RotHInput = value.x;
+        RotVInput = value.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(RotHInput) > 0.1f)
+        {
+            GO.transform.Rotate(Vector3.up, -RotHInput);
+        }
+
+        if (Mathf.Abs(RotVInput) > 0.1f)
+        {
+            GO.transform.Rotate(Vector3.right, RotVInput);
+        }
+    }
+}
