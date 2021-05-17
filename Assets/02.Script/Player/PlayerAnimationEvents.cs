0c5f3cb1e5b6fbef126bbac58bb1a62b0ce944d8
MinhwanKwak <rhkralsghks7@naver.com>
2021-05-18 05:50:48 +0900

    1
    1

--- Assets/02.Script/Player/PlayerAnimationEvents.cs
+++ Assets/02.Script/Player/PlayerAnimationEvents.cs
@@ -59,12 +59,13 @@ public class PlayerAnimationEvents : MonoBehaviour
         IsAnimStart = false;
         PlayerManager.Instance.playerMove.IsGravity = false;
     }
+    public void DownEnd()
+    {
         PlayerManager.Instance.playerMove.IsGravity = true;
         //PlayerManager.Instance.playerMove.ClimingJudgeDown();
         IsAnimStart = true;
     }
-    public void DownEnd()
-    {
+  
 
     public void DownStart()
     {
