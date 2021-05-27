3d78bec1c616ca607474606b9b163542f46e3d20
moonjyo <m_jyo@naver.com>
2021-05-27 22:07:12 +0900

    ui credit, ui size modulate / dispose

--- Assets/02.Script/InterActionObj/UI/UIPauseWindow.cs
+++ Assets/02.Script/InterActionObj/UI/UIPauseWindow.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIPauseWindow : UIView
 {
+    public GameObject CreditPanel;
+    public RawImage Credit;
+
     public void ShowCredit()
     {
-
+        CreditPanel.SetActive(true);
+        Credit.rectTransform.anchoredPosition = new Vector2(0, -2570f);
+        Credit.rectTransform.DOLocalMoveY(2570f, 20f).OnComplete(() => CreditPanel.SetActive(false));
     }
 }
